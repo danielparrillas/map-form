2f10896c2ab507c6348af7eb28b94a3c63e8dba6
Brandon Parrillas <brandonps.dev@gmail.com>
2023-06-13 18:30:22 -0600

    optimizacion

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,12 +1,16 @@
 // import UploadFile from "./UploadFile";
 import { GeometryContainer } from "./GeometryList.tsx";
+import GeometryForm from "./GeometryForm";
+import { graphicToFeature } from "../utils/feature";
+import { useMapStore } from "../map/store.ts";
 
 export default function SideBar() {
+  const { graphic, setGraphic } = useMapStore();
   return (
     <div className="p-4 flex flex-col w-96">
       <h1>Coordenadas</h1>
       {/* <UploadFile /> */}
-      <GeometryContainer />
+      <GeometryForm feature={graphicToFeature(graphic)} />
     </div>
   );
 }
