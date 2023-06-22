ec9fad231af9c7e8a01139924bef7d9ba98b6639
Brandon Parrillas <brandonps.dev@gmail.com>
2023-06-21 18:32:04 -0600

    clean code

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@
 //📒mine
 import Map from "./components/Map";
 import SideBar from "./components/SideBar";
-import Sheets from "./components/Sheets";
 
 function App() {
   return (
     <div className="overflow-hidden flex h-screen">
       <SideBar />
       <Map />
-      {/* <Sheets /> */}
     </div>
   );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,8 +4,6 @@ import LayerList from "./LayerList";
 export default function SideBar() {
   return (
     <div className="p-4 flex flex-col w-96 bg-stone-100 gap-2">
-      <h2>Formulario</h2>
-      {/* <UploadFile /> */}
       <LayerList />
       <GeometryForm />
     </div>
